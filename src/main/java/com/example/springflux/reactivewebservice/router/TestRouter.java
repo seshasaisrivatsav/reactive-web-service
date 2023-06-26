c1ddad2126598abeddbc2602e520c0673a534321
Sesha Sai Srivatsav Kuchibhotla <skuch007@hdqbeamn43v3f.attlocal.net>
2023-06-26 15:50:40 -0500

    websocket works

--- src/main/java/com/example/springflux/reactivewebservice/router/TestRouter.java
+++ src/main/java/com/example/springflux/reactivewebservice/router/TestRouter.java
@@ -1,9 +1,17 @@
 package com.example.springflux.reactivewebservice.router;
+import com.example.springflux.reactivewebservice.handler.MyWebSocketHandler;
 import com.example.springflux.reactivewebservice.handler.TestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -16,5 +24,30 @@ public class TestRouter {
         return route(GET("/item"), handler::getItem)
                 .andRoute(GET("/websockettest"), handler::websocketTest);
     }
+
+
+    // WebSocket Handler
+    @Bean
+    public MyWebSocketHandler myWebSocketHandler() {
+        return new MyWebSocketHandler();
+    }
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        Map<String, WebSocketHandler> map = new HashMap<>();
+        map.put("/websocket-endpoint", myWebSocketHandler());
+
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setUrlMap(map);
+        mapping.setOrder(1); // Set the order for the handler mapping
+        return mapping;
+    }
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+
 
 }
